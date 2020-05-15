34f8bb5e963d5b0116876e7d9c5f66eebeb45d42
Tyler Bauman <t.bauman@vehikl.com>
2020-05-15 15:40:42 -0400

    chore: add test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,17 @@
 module.exports = {
   testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
   transform: {
-    '^.+\\.(ts|js|html)$': 'ts-jest',
+    '^.+\\.(ts|js|html)$': 'ts-jest'
   },
   resolver: '@nrwl/jest/plugins/resolver',
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageReporters: ['html'],
+  coverageReporters: ['html', 'lcov', 'text'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -40
+    }
+  }
 };
