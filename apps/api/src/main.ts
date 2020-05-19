7ee9967391987330528399520d27ffac5b4d357e
Tyler Bauman <t.bauman@vehikl.com>
2020-05-19 09:50:55 -0400

    chore: set up swagger
    # Conflicts:
    #	apps/api/src/app/doughnuts/doughnuts.controller.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,19 +3,23 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
-  });
+
+  const options = new DocumentBuilder()
+    .setTitle('Doughnuts API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 
 bootstrap();
