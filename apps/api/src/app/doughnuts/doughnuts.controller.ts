3e716c208117201c72b0300e74090056492ddcc2
Tyler Bauman <t.bauman@vehikl.com>
2020-05-19 09:29:46 -0400

    chore: set up swagger

--- apps/api/src/app/doughnuts/doughnuts.controller.ts
+++ apps/api/src/app/doughnuts/doughnuts.controller.ts
@@ -1,6 +1,9 @@
 import { Doughnut } from '@doughnuts-and-dls/api-interfaces';
 import { Controller, Get, Param } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { DoughnutsService } from './doughnuts.service';
+
+@ApiTags('doughnuts')
 @Controller('doughnuts')
 export class DoughnutsController {
   constructor(private readonly doughnutsService: DoughnutsService) {}
